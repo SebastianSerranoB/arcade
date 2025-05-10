11a0ee791ae7b8da5edb0938e29216f37a2642cb
SebastianSerranoB <sebastianserranob11@gmail.com>
2025-05-10 08:57:07 -0300

    Commit post review sprint 3, previo a cambiar estructura de modulos

--- src/app/core/services/chat.service.ts
+++ src/app/core/services/chat.service.ts
@@ -3,6 +3,8 @@ import { createClient, RealtimeChannel, SupabaseClient } from '@supabase/supabas
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 
+
+//interfaz hace las veces de DTO
 export interface Message{
   id: string;
   content: string;
@@ -17,10 +19,17 @@ export class ChatService {
   private supabase: SupabaseClient;
   private channel: RealtimeChannel | null = null;
 
+  //Traigo mi instancia de supabase
   constructor( private auth: AuthService) { 
     this.supabase = auth.getClient();
   }
 
+
+   //Message[] dto, la lista de mensajes
+   // {data, error} es una forma de deconstruir un obj en js. 
+   //hago la query postrgres. 
+   //if error true retorno vacio.
+   //data son los message[]
   async fetchMessages(): Promise<Message[]>{
     const{ data, error } = await this.supabase
     .from('messages')
@@ -32,9 +41,11 @@ export class ChatService {
       return [];
     }
 
-    return data as Message[];
+    return data as Message[]; //forzamos el tipo de dato xq sabemos lo que retorna. type assertion
   }
 
+
+  //insertamos el msj en la db
   async sendMessage(content: string, userEmail: string): Promise<boolean>{
     const { error } = await this.supabase.from('messages').insert([
       {
@@ -52,6 +63,14 @@ export class ChatService {
   }
 
 
+
+
+  //RealTimeChannel channel es un canal con la DB.
+  //.on esta escuchando el evento INSERT en public.messages
+  //payload es el mensaje nuevo
+  //retorno el channel para que el componente maneje la instancia y termine la subscipcion.
+  //Esto es lo importante, recibe una callback function(onNewMessage), cada vez que se inserta un msj en la db se ejecuta.
+  //la callback que mando desde el componente, pushea el mensaje a un array de msjs que se muestran en el html.
   subscribeToMessages(onNewMessage: (msg: Message) => void): RealtimeChannel{
     this.channel = this.supabase.channel('messages-channel')
     .on('postgres_changes',{
@@ -70,6 +89,8 @@ export class ChatService {
 
 
 
+
+  //Esto creo que corresponde en chatComponent(no aca en el servicio, salvo que mande la instancia). Es para liberar la memoria.
   removeSubscription(): void{
     if(this.channel){
       this.supabase.removeChannel(this.channel);
